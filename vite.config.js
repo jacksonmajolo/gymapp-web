fb560a6b83e747cf6731aa23de67f51dbe602237
Jackson D. Majolo <jacksonmajolo@gmail.com>
2025-11-19 16:58:00 -0300

    init vite + react

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return defineConfig({
+    plugins: [
+      react()
+    ],
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@admin': path.resolve(__dirname, './src/admin/src'),
+        '@client': path.resolve(__dirname, './src/client/src'),
+        '@public': path.resolve(__dirname, './src/public/src'),
+        '@shared': path.resolve(__dirname, './src/shared/src'),
+      },
+    },
+
+    server: {
+      host: true,
+      allowedHosts: ['host.docker.internal'],
+      port: Number(env.VITE_PORT) || 5173,
+    },
+  })
+}
